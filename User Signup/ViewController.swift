259d8d193e7750ab23367d69af69f0afa26ce74f
Dev Soni <iamdevsoni@gmail.com>
2024-02-17 11:13:05 +0530

    Added Alert message. Implemented concept of ViewController lifestyle.

--- User Signup/ViewController.swift
+++ User Signup/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var birthdayLabel: UILabel!
     var storedName = ""
     var userName = ""
+    var birthDay = ""
     override func viewDidLoad() {
         super.viewDidLoad()
         let storedName = UserDefaults.standard.object(forKey: "name")
@@ -25,13 +26,30 @@ class ViewController: UIViewController {
         }
         if let myBirthday = storedBirthday as? String {
             birthdayLabel.text = "Birthday: \(myBirthday)"
+            birthDay = myBirthday
         }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        nameLabel.text = "Name: \(userName)"
+        birthdayLabel.text = "Birthday: \(birthDay)"
     }
 
     @IBAction func saveBtn(_ sender: Any) {
         
+        let alert = UIAlertController(title: "Error!", message: "Name can't be empty!", preferredStyle: UIAlertController.Style.alert)
+        
+        
         UserDefaults.standard.set(nameText.text!, forKey: "name")
         UserDefaults.standard.set(birthdayText.text!, forKey: "birthday")
+        
+        if UserDefaults.standard.object(forKey: "name") as? String == ""{
+            self.present(alert, animated: true, completion: nil)
+            let okBtn = UIAlertAction(title: "OK", style: UIAlertAction.Style.default) { UIAlertAction in
+                // Btn Clicked
+            }
+            alert.addAction(okBtn)
+        }
         
         nameLabel.text = "Name: \(nameText.text!)"
         birthdayLabel.text = "Birthday: \(birthdayText.text!)"
